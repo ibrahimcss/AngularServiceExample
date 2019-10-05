4a9d8309d317b64d3125a3ee719aa62a6204975b
ibrahimcss <ibrahimcss@outlook.com>
2019-10-05 16:28:02 +0300

    Angular Using Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { CryptoService } from './crypto.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularServiceExample';
+  coins = [];
+  constructor(private cryptoservice: CryptoService)
+  {
+    this.coins = cryptoservice.getMyItems();
+  }
 }
